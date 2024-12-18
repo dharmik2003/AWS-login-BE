e0d91a14db3b0d4933370954bc9089c6e8e173cf
Dharmik Kachhadiya <99231778+dharmik2003@users.noreply.github.com>
2024-12-18 16:50:33 +0530

    Update userRoutes.ts

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,13 +1,12 @@
 import express, { Request, Response } from "express";
 import { getUserList, Login, Signup } from "../controllers/userController";
-import { Validate } from "../../src/middlewares//validate-request.middlewares";
 import { signupValidator } from "../validators/auth/signup.validator";
 import { loginValidator } from "../validators/auth/login.validators";
 
 const router = express.Router();
 
-router.post("/signup", Validate(signupValidator), Signup);
-router.post("/login", Validate(loginValidator), Login);
+router.post("/signup", signupValidator, Signup);
+router.post("/login", loginValidator, Login);
 router.get("/users", getUserList);
 
 router.get("/", (req, res) => {
